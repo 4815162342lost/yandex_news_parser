23e16ca205cecaefb0f76f650caeb64eae27d0f9
4815162342lost <family-gan@yandex.ru>
2018-08-18 18:31:17 +0300

    Update yanews.py

--- yanews.py
+++ yanews.py
@@ -5,8 +5,8 @@ r=requests.get("http://yandex.ru")
 soup=BeautifulSoup(r.text, "html.parser")
 news=soup.find_all(class_="home-link list__item-content home-link_black_yes")
 print("Global news:")
-for new_cur in news[0:5]:
+for new_cur in news[0:10]:
         print(new_cur.get("aria-label"))
 print("\nCity news:")
-for new_cur in news[5:10]:
+for new_cur in news[10:20]:
         print(new_cur.get("aria-label"))
