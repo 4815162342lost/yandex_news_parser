52ccae8fef65661be71e64d573dd4643a711adbd
4815162342lost <family-gan@yandex.ru>
2022-01-24 23:11:18 +0300

    Update yanews.py

--- yanews.py
+++ yanews.py
@@ -2,6 +2,8 @@
 import requests
 from bs4 import BeautifulSoup
 r=requests.get("http://yandex.ru")
+
+"""old version
 soup=BeautifulSoup(r.text, "html.parser")
 news=soup.find_all(class_="home-link list__item-content home-link_black_yes")
 print("Global news:")
@@ -10,3 +12,11 @@ for new_cur in news[0:10]:
 print("\nCity news:")
 for new_cur in news[10:20]:
         print(new_cur.get("aria-label"))
+"""
+        
+soup=BeautifulSoup(r.text, "html.parser")
+news=soup.find_all(class_="news__item-content")
+
+print("Russian news:")
+for current_news in news:
+    print(current_news.text)
